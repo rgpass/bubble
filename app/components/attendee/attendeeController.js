74260f4dcb454eacd5ad19d82379fba9ec2aa648
rgpass <rgpass@gmail.com>
2015-04-11 15:35:30 -0400

    Add functionality

--- app/components/attendee/attendeeController.js
+++ app/components/attendee/attendeeController.js
@@ -1,3 +1,90 @@
 app.controller('AttendeeCtrl', function($scope, $firebaseObject, $firebaseArray) {
-  $scope.query = 'From AttendeeCtrl';
+  // var refObject = new Firebase("https://getbubble.firebaseio.com/");
+
+  // var syncObject = $firebaseObject(refObject);
+
+  // syncObject.$bindTo($scope, "attendee");
+
+  $scope.printStuff = function() {
+    console.log($scope.attendee);
+  }
+
+  // var refArray = new Firebase("https://getbubble.firebaseio.com/attendees");
+  // $scope.attendees = $firebaseArray(refArray);
+
+  // $scope.addAttendee = function() {
+  //   $scope.attendees.$add($scope.attendee);
+  // }
+
+  // $scope.saveAttendee = function(attendee) {
+  //   var attendee = attendee || $scope.attendee;
+  //   $scope.attendees.$save(attendee);
+  // }
+
+  // CREATE USER ON LOAD
+  var randomKey = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 8);
+  var defaultAttendee = { name: '', overall: 'good', speed: 0, volume: 0, key: randomKey };
+  var refObject = new Firebase("https://getbubble.firebaseio.com/attendees/" + defaultAttendee.key);
+  var syncObject = $firebaseObject(refObject);
+  refObject.set(defaultAttendee);
+  syncObject.$bindTo($scope, "attendee");
+
+
+  // LOAD IN USER'S QUESTIONS
+  var refArray = new Firebase("https://getbubble.firebaseio.com/attendees/" + defaultAttendee.key + '/questions');
+  $scope.questions = $firebaseArray(refArray);
+
+  $scope.addQuestion = function() {
+    $scope.questions.$add({
+      content: $scope.questionContent
+    });
+  };
+
+  $scope.setToAnswered = function(question) {
+    question.answered = true;
+    $scope.questions.$save(question);
+  };
+
+
+  // var refArray = new Firebase("https://getbubble.firebaseio.com/attendees");
+  // $scope.attendees = $firebaseArray(refArray);
+
+  // $scope.attendees.$add($scope.attendee);
+
+
+
+  // $scope.$watch('attendee', function(newValue, oldValue) {
+  //   var attendee = _.find($scope.attendees, function(attendee) {
+  //     return attendee.key == randomKey;
+  //   });
+  //   $scope.attendees.$save(attendee);
+  // })
+
+  // On load
+    // Create attendee object with defaults
+      // Sidebar: disable buttons that are already clicked
+    // Save to firebase
+
+  // On click for speed
+    // Set speed to the value
+      // 1 for too fast
+      // 0 for good
+      // -1 for too slow
+
+  // On click for volume
+    // Set volume to the value
+      // 1 for too quiet
+      // 0 for good
+      // -1 for too loud
+
+  // On question submit
+    // Add to questions array
+      // content:string(don't push if blank), approved:boolean(false), answered:boolean(false)
+        // TODO: Moderator functionality?
+
+  // On question remove
+    // Remove from questions array
+
+  // On question answered
+    // Set question's answered attribute to true
 });
--- app/components/presenter/presenterController.js
+++ app/components/presenter/presenterController.js
@@ -1,3 +1,66 @@
 app.controller('PresenterCtrl', function($scope, $firebaseObject, $firebaseArray) {
-  $scope.query = 'From PresenterCtrl';
+
+  // On load
+  var refArray = new Firebase("https://getbubble.firebaseio.com/attendees");
+  
+  $scope.attendees = $firebaseArray(refArray);
+
+  $scope.$watch('attendees', function(newVal, oldVal) {
+    var numAttendees = $scope.attendees.length
+
+    var lostAttendees = _.filter($scope.attendees, function(attendee) {
+      return attendee.overall == 'lost';
+    })
+    $scope.lostPercent = Math.round(lostAttendees.length * 100 / numAttendees);
+
+    var speedValue = _.reduce($scope.attendees, function(memo, attendee) {
+      return memo + attendee.speed;
+    }, 0);
+    $scope.speedStatus = speedStatus(Math.round(speedValue * 100 / numAttendees));
+
+    var volumeValue = _.reduce($scope.attendees, function(memo, attendee) {
+      return memo + attendee.volume;
+    }, 0);
+    $scope.volumeStatus = volumeStatus(Math.round(volumeValue * 100 / numAttendees));
+
+    $scope.questions = [];
+    _.each($scope.attendees, function(attendee) {
+      _.each(attendee.questions, function(question) {
+        $scope.questions.push({ name: attendee.name, content: question.content, answered: question.answered })
+      });
+    });
+  }, true);
+
+  
+
+
+  /***** UTILITY FUNCTIONS *****/
+  function speedStatus(percent) {
+    if (percent > 20) {
+      return { content: 'SLOW DOWN!', class: 'text-danger' };
+    } else if (percent > 10) {
+      return { content: 'Little slower', class: 'text-warning' };
+    } else if (percent < -20) {
+      return { content: 'GO FASTER!', class: 'text-danger' };
+    } else if (percent < -10) {
+      return { content: 'Little faster', class: 'text-warning' };
+    } else {
+      return { content: 'Good', class: 'text-success' };
+    }
+  }
+
+  function volumeStatus(percent) {
+    if (percent > 10) {
+      return { content: 'SPEAK UP!', class: 'text-danger' };
+    } else if (percent > 5) {
+      return { content: 'Little louder', class: 'text-warning' };
+    } else if (percent < -10) {
+      return { content: 'SHH!!', class: 'text-danger' };
+    } else if (percent < -5) {
+      return { content: 'Little quieter', class: 'text-warning' };
+    } else {
+      return { content: 'Good', class: 'text-success' };
+    }
+  }
+
 });
